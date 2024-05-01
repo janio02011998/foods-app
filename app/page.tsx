9bfcdfa90d420db460c19c7efd8f102532400bcf
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-04-30 21:51:07 -0300

    feat: add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -11,6 +12,17 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src={"promo-banner-01.png"}
+          alt={"Até 30% de desconto em pizzas"}
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
