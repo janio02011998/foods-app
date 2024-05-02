992097bb1996a0fe66bc15d669a0141f0a057c48
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-02 16:10:24 -0300

    fix: header logo image

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,9 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-6 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
+      <div className="relative h-[30px] w-[100px]">
+        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
+      </div>
       <Button
         size={"icon"}
         variant={"outline"}
