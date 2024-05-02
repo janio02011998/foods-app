5e4f8bd4db280d5768ce84b371ce80696b53144a
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-02 15:59:24 -0300

    feat: add recommend restaurant page

---
+++ app/restaurants/recommended/page.tsx
@@ -0,0 +1,26 @@
+import Header from "@/app/_components/header";
+import RestaurantItem from "@/app/_components/restaurant-item";
+import { db } from "@/app/_lib/prisma";
+
+const RecommendedRestaurants = async () => {
+  const restaurants = await db.restaurant.findMany({});
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className=" mb-6 text-lg font-semibold">Restaurantes Favoritos</h2>
+        <div className="flex w-full flex-col gap-6">
+          {restaurants.map((restaurant) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecommendedRestaurants;
