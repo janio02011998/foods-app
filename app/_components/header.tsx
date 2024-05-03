9b418377228481043a323cccc59f772a2fb20a79
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-05-03 07:31:27 -0300

    feat: add link in header logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-6 pt-6">
       <div className="relative h-[30px] w-[100px]">
-        <Image src="/logo.png" alt="FSW Foods" fill className="object-cover" />
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="FSW Foods"
+            fill
+            className="object-cover"
+          />
+        </Link>
       </div>
       <Button
         size={"icon"}
