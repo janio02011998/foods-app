e2a4f6f66b6011524fff96146d43e27728741557
Janio Carvalho <janiocarvalhojr@gmail.com>
2024-07-09 10:51:03 -0300

    refactor: add price and discount on cart-item

--- app/_components/cart-item.tsx
+++ app/_components/cart-item.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { CartProduct } from "../_contexts/cart";
+import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 
 interface CartItemsProps {
   cartProduct: CartProduct;
@@ -8,14 +9,33 @@ interface CartItemsProps {
 const CartItem = ({ cartProduct }: CartItemsProps) => {
   return (
     <div className="flex items-center justify-between">
-      {/* IMAGE AND INFO */}
-      <div className="relative h-20  w-20 ">
-        <Image
-          fill
-          className="rounded-lg object-cover"
-          src={cartProduct.imageUrl}
-          alt={cartProduct.name}
-        />
+      <div className="flex items-center gap-4">
+        {/* IMAGE AND INFO */}
+        <div className="relative h-20 w-20 ">
+          <Image
+            fill
+            className="rounded-lg object-cover"
+            src={cartProduct.imageUrl}
+            alt={cartProduct.name}
+          />
+        </div>
+
+        <div className="space-y-1">
+          <h3 className="text-xs">{cartProduct.name}</h3>
+
+          <div className="flex items-center gap-1">
+            <h4 className="text-sm font-semibold">
+              {formatCurrency(calculateProductTotalPrice(cartProduct))}
+            </h4>
+            {cartProduct.discountPercentage > 0 && (
+              <span className="text-xs text-muted-foreground line-through">
+                {formatCurrency(Number(cartProduct.price))}
+              </span>
+            )}
+          </div>
+
+          {/* QUANTITY */}
+        </div>
       </div>
 
       {/* BTN DELETE */}
